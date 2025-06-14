1b7e0197039287e794151de857f6edc719f8e2c3
amongus <adi4soni@gmail.com>
2025-06-14 16:27:23 -0600

    editted app

--- notes/part3 - Programming a server with NodeJS and Express/notesserver/index.js
+++ notes/part3 - Programming a server with NodeJS and Express/notesserver/index.js
@@ -38,7 +38,7 @@ let notes = [
 ]
 
 app.get('/', (request, response) => {
-  response.send('<h1>Hello World!</h1>')
+  response.send('<h1>HI PARTH</h1>')
 })
 
 app.get('/api/notes', (request, response) => {
