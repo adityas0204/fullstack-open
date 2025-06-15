95cc15c8140511533e80e46a188b237b4643d6ca
amongus <adi4soni@gmail.com>
2025-06-15 17:49:58 -0600

    completing exercise 3b

--- exercises/part3/phonebookserver/index.js
+++ exercises/part3/phonebookserver/index.js
@@ -3,6 +3,7 @@ const express = require('express')
 const app = express()
 
 app.use(express.json())
+app.use(express.static('dist'))
 
 morgan.token('body', (req) => {
 	if (req.body && Object.keys(req.body).length > 0) {
@@ -10,8 +11,6 @@ morgan.token('body', (req) => {
 	}
 	return ''
 })
-
-
 app.use(morgan('tiny :body'))
 
 persons = [
@@ -100,7 +99,7 @@ app.post('/api/persons', (request, response) => {
     response.json(person)
 })
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
     console.log(`listening to port ${PORT}`)
 })
