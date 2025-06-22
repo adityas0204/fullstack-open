8bf9ef4a35972fd8d5b356dd1e9ed599a31be264
Aditya Soni <adi4soni@gmail.com>
2025-06-21 21:24:35 -0600

    mid way through phonebook mongo.js

--- exercises/part3/phonebookserver/mongo.js
+++ exercises/part3/phonebookserver/mongo.js
@@ -1,12 +1,43 @@
 const mongoose = require('mongoose')
 
 if (process.argv.length < 3) {
-    console.log('please provide a password as an argument')
+    console.log('please provide a password as an arguments')
     process.exit(1)
 }
 
 const password = process.argv[2]
 
-const url = `mongodb+srv://phonebook:${password}@cluster0.bydncdp.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`
+const url = `mongodb+srv://phonebook:${password}@cluster0.bydncdp.mongodb.net/phonebookApp?retryWrites=true&w=majority&appName=Cluster0`
 
 mongoose.set('strictQuery', false)
+
+mongoose.connect(url)
+
+const contactSchema = new mongoose.Schema({
+    name: String, 
+    number: String
+})
+
+const Contact = mongoose.model('Contact', contactSchema)
+
+if (process.argv.length === 3) {
+    console.log('phonebook:')
+    Contact.find({}).then(result => {
+        result.forEach(contact => console.log(contact.name, contact.number))
+        mongoose.connection.close()
+    })
+} else {
+    const name = process.argv[3]
+    const number = process.argv[4]
+
+    const contact = new Contact({
+        name: name,
+        number: number
+    })
+
+    contact.save().then(response => {
+        console.log(`added ${name} number ${number} to phonebook`)
+        mongoose.connection.close()
+    })
+}
+
