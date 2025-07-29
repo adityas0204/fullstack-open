211c7ac27e4d74f709ee1783a4dfb9580b0160bb
amongus <adi4soni@gmail.com>
2025-07-29 17:22:10 -0600

    creating scripts

---
+++ tools/indexCreation.py
@@ -0,0 +1,17 @@
+import os
+
+base_path = './notes'
+max_depth = 1
+index = open('./tools/index.txt', 'w')
+
+for root, dirs, files in os.walk(base_path):
+    rel_path = os.path.relpath(root, base_path)
+    print(rel_path, rel_path.count(os.sep))
+    depth = 0 if rel_path == '.' else rel_path.count(os.sep) + 1
+
+    if depth >= max_depth:
+        dirs.clear()
+
+    for file in files:
+        if file.endswith('.md'):
+            index.write((os.path.join(root, file) + '\n').replace("\\", "/"))
